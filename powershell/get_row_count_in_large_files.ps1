70917f4e8ff759e3fa0dd738f50600bbb9f2ab8e
atepavicharov <atepavicharov@gmail.com>
2022-08-03 16:25:47 +0300

    Update get_row_count_in_large_files.ps1

--- powershell/get_row_count_in_large_files.ps1
+++ powershell/get_row_count_in_large_files.ps1
@@ -1,7 +1,7 @@
 # Get-Content and Measure-Object are fine for small files, but both are super inefficient with memory. 
 # I had real problems with large files.
 
-$fileName = Get-ChildItem "D:\Downloads\Instantcash\ICA_aging\"
+$fileName = Get-ChildItem "<path_to_folder>\"
 
 $fileName | ForEach-Object {
 
