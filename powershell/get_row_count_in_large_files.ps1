94f56d692f520006a578f73404b34be9430e6309
atepavicharov <atepavicharov@gmail.com>
2022-08-03 16:25:19 +0300

    Create get_row_count_in_large_files.ps1

---
+++ powershell/get_row_count_in_large_files.ps1
@@ -0,0 +1,15 @@
+# Get-Content and Measure-Object are fine for small files, but both are super inefficient with memory. 
+# I had real problems with large files.
+
+$fileName = Get-ChildItem "D:\Downloads\Instantcash\ICA_aging\"
+
+$fileName | ForEach-Object {
+
+    [int]$LinesInFile = 0
+    $reader = New-Object IO.StreamReader $_.FullName
+     while($reader.ReadLine() -ne $null){ $LinesInFile++ }
+
+    Write-Host -ForegroundColor Magenta $_.FullName":"($LinesInFile-1) # removing the header
+
+    $reader.Dispose()
+}
