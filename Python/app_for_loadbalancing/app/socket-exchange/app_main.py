866704c3825786a1013912c72141b7e2cb26ec3e
DESKTOP-C39I041\ANGELT <angel@finbond.com>
2024-06-04 10:12:17 +0300

    - adding comment

--- Python/app_for_loadbalancing/app/socket-exchange/app_main.py
+++ Python/app_for_loadbalancing/app/socket-exchange/app_main.py
@@ -1,5 +1,6 @@
 import socket
 
+# based on https://stackoverflow.com/questions/7749341/basic-python-client-socket-example
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 8089))
 serversocket.listen(5)  # become a server socket, maximum 5 connections
